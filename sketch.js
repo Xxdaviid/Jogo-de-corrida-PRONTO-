58396476b4661979c5e53ac8751fe3a60b0f450a
Xxdaviid <93736528+Xxdaviid@users.noreply.github.com>
2022-03-31 16:51:15 -0300

    31/03/2022

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage, lifeImage;
+var obstacle1Image, obstacle2Image;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles;
+var cars = [];
+var FINAL;
+function preload() {
+  backgroundImage = loadImage("./assets/pista maior.png");
+  car1_img = loadImage("car1.png");
+  car2_img = loadImage("car2.png");
+  track = loadImage("PISTA.png");
+  fuelImage = loadImage("./assets/fuel.png");
+  powerCoinImage = loadImage("./assets/goldCoin.png");
+  obstacle1Image = loadImage("./assets/obstacle1.png");
+  obstacle2Image = loadImage("./assets/obstacle2.png");
+  lifeImage = loadImage("./assets/life.png");
+  FINAL = loadImage("./assets/blast.png");
+}
+
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+
+  if (gameState === 2) {
+    game.showLeaderboard();
+    game.acabou();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
